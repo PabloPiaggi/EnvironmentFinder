95505ce4b36306f4539b23171ce6d6a7154b4776
PabloPiaggi <ppiaggi@gmail.com>
2020-04-01 11:57:57 -0400

    Add docstrings and clean up

--- environmentfinder.py
+++ environmentfinder.py
@@ -17,20 +17,28 @@ class Environments:
         self.indeces = np.array([],dtype=np.int)
         self.delta = np.array([])
         self.distance =  np.array([])
-        
+
 # Class to find environments
 class EnvironmentFinder:
-    
+
     def __init__(self):
+        # self.fractionalFlag is True if the coordinates are fractional
         self.fractionalFlag=False
+        # uniqueFlags is True if unique environments are to be calculated
         self.uniqueFlag=True
+        # self.fastFlag is True if the fast algorithm is to be employed
         self.fastFlag=True
+        # self.allEnvs stores all calculated environments
         self.allEnvs=np.ndarray((0,),dtype=np.object)
+        # self.uniqueEnvs stores the unique environments
         self.uniqueEnvs = np.ndarray((0,),dtype=np.object)
+        # self.tolerance is the tolerance to determine if two elements of the
+        # distance vectors are the same
         self.tolerance=0
         #env=Environments()
         #env.delta.shape = (0,3)
         #uniqueEnvs = np.append(uniqueEnvs,env)
+        # self.box_layout contains options to display a box
         self.box_layout = widgets.Layout(display='flex',
                     flex_flow='column',
                     align_items='stretch',
@@ -38,25 +46,58 @@ class EnvironmentFinder:
                     width='90%')
 
     def plotConf(self):
+        """ Plot configuration using nglview """
+
         v = nglview.show_ase(self.conf)
         v.add_representation("unitcell")
         boxy=widgets.Box([v],layout=self.box_layout)
         display(boxy)
 
-    def chooseConfiguration(self,Configuration):
-        filename=Configuration
+    def chooseConfiguration(self,filename):
+        """ Choose configuration by filename
+
+        Args:
+            filename (string): the file name
+
+        Returns:
+            no value
+        """
+
         self.conf = aseio.read(filename)
         self.atom_types = np.unique(self.conf.get_chemical_symbols())
 
-    def chooseAndPlotConfiguration(self,Configuration):
-        self.chooseConfiguration(Configuration)
+    def chooseAndPlotConfiguration(self,filename):
+        """ Choose and plot configuration by filename
+
+        Args:
+            filename (string): the file name
+
+        Returns:
+            no value
+        """
+
+        self.chooseConfiguration(filename)
         self.plotConf()
-        
+
     def calculateEnvironments(self,lista,listb,cutoff):
+        """ Find environments
+
+        This function calculates all environments centered at the atom in
+        lista using atoms in listb as neighbors if they are within the cutoff.
+
+        Args:
+            lista (numpy vector): list of central atoms
+            listb (numpy vector): list of neighboring atoms
+            cutoff (float): cut off for the calculation of neighbors
+
+        Returns:
+            no value
+        """
+
         self.allEnvs = np.ndarray((0,),dtype=np.object)
         # Find all environments
         # Loop over input particles of type atom_type_1:
-        myindeces=lista 
+        myindeces=lista
         nl = asenl.NeighborList((cutoff/2.0)*np.ones(self.conf.get_number_of_atoms()),skin=0.1,bothways=True)
         nl.update(self.conf)
         mat = self.conf.get_cell()
@@ -80,6 +121,26 @@ class EnvironmentFinder:
             self.allEnvs = np.append(self.allEnvs,Environment)
 
     def CalculateUniqueEnvironments(self):
+        """ Calculate unique Environments
+
+        This algorithm to calculate unique environments compares all possible
+        permutations of the environments. It is thus always correct but often
+        very slow.
+
+        The number of operations scales roughly as as N^2 * M! where
+        N is the total number of environments and M is the number of atoms in
+        the environments.
+
+        CalculateUniqueEnvironmentsFast is a faster alternative to this
+        function.
+
+        Args:
+            none
+
+        Returns:
+            no value
+        """
+
         num_of_templates=self.allEnvs.shape[0]
         flag_unique=np.ones(num_of_templates)
         # Disregard empty environments
@@ -105,6 +166,27 @@ class EnvironmentFinder:
                 self.uniqueEnvs = np.append(self.uniqueEnvs,self.allEnvs[i])
 
     def CalculateUniqueEnvironmentsFast(self):
+        """ Calculate unique Environments
+
+        This algorithm to calculate unique environments first sorts the
+        distance vectors (central atom to neighbor vectots) and then compares
+        them. The algorithm is fast but it can be wrong in some pathological
+        cases.
+
+        The number of operations scales roughly as as N^2 * M * log(M)
+        where N is the total number of environments and M is the number of
+        atoms in the environments.
+
+        CalculateUniqueEnvironments is a much slower alternative to this
+        function but it is always correct.
+
+        Args:
+            none
+
+        Returns:
+            no value
+        """
+
         # This is a fast but sometimes wrong algorithm to compare environments
         num_of_templates=self.allEnvs.shape[0]
         # Sort according to x+y+z,z,y,x
@@ -145,15 +227,15 @@ class EnvironmentFinder:
         num_of_templates=Environments.shape[0]
         for i in range(num_of_templates):
             env_atom_types = np.asarray(self.conf.get_chemical_symbols())[Environments[i].indeces.astype(int)]
-            env_positions = Environments[i].delta*10 
+            env_positions = Environments[i].delta*10
             env = ase.Atoms(env_atom_types,env_positions)
             aseio.write('-', env, format='proteindatabank')
 
-                
-    def plotEnv(self,myEnvironment): 
+
+    def plotEnv(self,myEnvironment):
         env_atom_types = np.asarray(self.conf.get_chemical_symbols())[myEnvironment.indeces.astype(int)] #,np.array('C')] #Template[env_number].myindex)]
         env_atom_types = np.append(env_atom_types,np.array('Si'))
-        env_positions = np.vstack((myEnvironment.delta*10,np.array([0,0,0]) ) ) 
+        env_positions = np.vstack((myEnvironment.delta*10,np.array([0,0,0]) ) )
         env = ase.Atoms(env_atom_types,env_positions)
         v = nglview.show_ase(env)
         boxy=widgets.Box([v],layout=self.box_layout)
@@ -164,11 +246,11 @@ class EnvironmentFinder:
             self.plotEnv(self.allEnvs[number-1])
         else:
             print("Error")
-    
+
     def chooseEnvPlotUnique(self,number):
         if (self.uniqueEnvs.shape[0]>0):
             self.plotEnv(self.uniqueEnvs[number-1])
-        
+
     def chooseEnvPlot(self,number):
         if (self.uniqueFlag and self.uniqueEnvs.shape[0]>0):
             self.chooseEnvPlotUnique(number)
@@ -205,7 +287,7 @@ class EnvironmentFinder:
             self.printEnvironmentSummaryInfo(self.uniqueEnvs)
         else:
             self.printEnvironmentSummaryInfo(self.allEnvs)
-    
+
     def calculateEnvironmentsMinMaxStride(self,mina,maxa,stridea,minb,maxb,strideb,cutoff,tolerance):
         self.tolerance=tolerance
         lista=np.arange(int(mina),int(maxa)+1,int(stridea))-1
@@ -220,4 +302,3 @@ class EnvironmentFinder:
             self.printEnvironmentSummaryInfo(self.uniqueEnvs)
         else:
             self.printEnvironmentSummaryInfo(self.allEnvs)
-    
